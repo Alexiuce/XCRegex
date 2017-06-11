334723ec9932817a09bd3dc133899ea0fdaeb1dc
秀策 <xiuce@naitang.com>
2017-06-11 15:22:31 +0800

    placeholder text

--- XCRegex/XCRexWindow.m
+++ XCRegex/XCRexWindow.m
@@ -29,11 +29,13 @@
     // 设置UI
     _patternTextView.font = [NSFont systemFontOfSize:15];
     _patternTextView.delegate = self;
-    _patternTextView.textColor = [NSColor whiteColor]; //;
+    _patternTextView.textColor = [NSColor whiteColor];
+    _patternTextView.automaticQuoteSubstitutionEnabled = NO;
     
     _sourceTextView.font = [NSFont systemFontOfSize:15];
     _sourceTextView.delegate = self;
     _sourceTextView.textColor = [NSColor whiteColor];
+    _sourceTextView.automaticQuoteSubstitutionEnabled = NO;
     // 设置状态栏icon
     self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
     self.statusItem.image = [NSImage imageNamed:@"statusItem"];
@@ -46,8 +48,11 @@
     self.styleMask = self.styleMask | NSFullSizeContentViewWindowMask;
     // 隐藏全屏按钮
     [self standardWindowButton:NSWindowZoomButton].hidden = YES;
+    [self textDidChange:[NSNotification notificationWithName:@"begin" object:nil]];
 
 }
+
+
 #pragma mark - 事件监听
 - (void)clickStatusItem{
     self.visible ? nil :[self makeKeyAndOrderFront:nil];
